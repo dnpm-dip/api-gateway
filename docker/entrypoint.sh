d0048a49a23accd26f44c5e6963a1d4b0dd5db36
Lucien Clin <lucien.clin@uni-tuebingen.de>
2024-11-11 14:25:27 +0100

    fix: Moved start-up commands into dedicated entrypoint.sh to remove 'JSONArgsRecommended' warning in image creation

---
+++ docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+/opt/$BACKEND_APP/bin/dnpm-dip-api-gateway \
+  -Dhttp.port="$HTTP_PORT" \
+  -Dplay.http.secret.key="$APPLICATION_SECRET" \
+  -Dconfig.file="$CONFIG_DIR/production.conf" \
+  -Dpidfile.path="$PID_FILE" \
+  -Dlogger.file="$CONFIG_DIR/logback.xml" \
+  -Ddnpm.dip.site="$LOCAL_SITE" \
+  -Ddnpm.dip.config.file="$CONFIG_DIR/config.xml" \
+  -Ddnpm.dip.authup.url="$AUTHUP_URL" \
+  -Ddnpm.dip.rest.api.baseurl="$HATEOAS_HOST" \
+  -Ddnpm.dip.connector.type="$CONNECTOR_TYPE" \
+  -Ddnpm.dip.data.dir="$DATA_DIR" \
+  -Ddnpm.dip.rd.query.data.generate="$RD_RANDOM_DATA" \
+  -Ddnpm.dip.mtb.query.data.generate="$MTB_RANDOM_DATA"
